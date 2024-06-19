7989b5c801c4a2f7a9ad9f3768cad490d3a4733a
Justin Hull <hull.justin@gmail.com>
2024-06-19 19:48:35 -0400

    no multer

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,11 @@
 const { Router } = require("express");
 const recipesController = require("../controllers/recipesController");
-const multer = require("multer");
-const upload = multer({ dest: "/images" });
 const app = Router();
 
 app.get("/recipes", recipesController.getRecipes);
 app.get("/recipesList", recipesController.getRecipesList);
 app.get("/recipes/:id", recipesController.findById);
 app.post("/recipes", recipesController.create);
-app.post("/recipes/:id", upload.single("file"), recipesController.updateImage);
+app.post("/recipes/:id", recipesController.updateImage);
 
 module.exports = app;
